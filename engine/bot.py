986642f9304704783844a64ef53a04ff0826cae8
Fakesum <worldanvilbild@gmail.com>
2022-11-30 19:55:59 +0530

    Fix

--- engine/bot.py
+++ engine/bot.py
@@ -1,7 +1,10 @@
 import threading as __threading
 class Bot(__threading.Thread):
-    def __init__(self) -> None:
+    def __init__(self, colab: bool=True, logging=True) -> None:
         self.__display = None
+
+        self.colab: bool = colab
+        self.logging = logging
 
         from .archive import active_subreddits
         self.active_subreddits = active_subreddits
@@ -9,16 +12,17 @@ class Bot(__threading.Thread):
         super().__init__()
         self.daemon = True
 
-    def run(self):
+    def run(self) -> None:
         import undetected_chromedriver as uc
         from selenium.webdriver.common.by import By
         from selenium.webdriver.common.action_chains import ActionChains
         from .utils import wrap_poll, exec_cmd, wrap_filter
 
         from loguru import logger
+        if not self.logging:
+            logger.remove()
         logger.info("Started")
 
-        @wrap_poll(None, poll=None, return_val=True, fast=True, error_logger=logger.warning)
         def init_driver():
             import os
             if os.name == "posix":
@@ -27,48 +31,45 @@ class Bot(__threading.Thread):
                 self.__display.start()
             
             options = uc.ChromeOptions()
-            options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--mute-audio")
             options.add_argument("--disable-notifications")
 
-            return uc.Chrome(options=options, version_main=int(exec_cmd("google-chrome-stable --version")[0].split(" ")[-2].split(".")[0]), no_sandbox=False)
+            if self.colab:
+                return uc.Chrome(
+                    options=options,
+                    version_main= int(exec_cmd("google-chrome-stable --version")[0].split(" ")[-2].split(".")[0]), # ex, 106 or 107
+                    no_sandbox=False
+                )
+            else:
+                return uc.Chrome(options=options, no_sandbox=False, driver_executable_path="/usr/bin/chromedriver")
         
         @wrap_filter
-        @wrap_poll(None, poll=None, return_val=True, fast=True, on_failer=(lambda: driver.switch_to.default_content()), error_logger=logger.warning)
+        @wrap_poll(None, poll=None, return_val=True, fast=True, on_failer=(lambda: driver.switch_to.default_content()), error_logger=print)
         def play_videos(video, driver: uc.Chrome):
             driver.execute_script("""arguments[0].scrollIntoView();""", video)
             
-            logger.debug(("-"*5)+"Point"+("-"*5))
-
             driver.switch_to.frame(video)
             driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, 'iframe'))
-
-            logger.debug(("-"*5)+"Point"+("-"*5))
             
             ActionChains(driver).move_to_element(driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')).click().perform()
+            logger.debug("Played Video")
             driver.switch_to.default_content()
 
-            logger.debug(("-"*5)+"Point"+("-"*5))
         
         driver = init_driver()
         logger.info("Driver Created")
 
 
         for rep in self.active_subreddits:
-            logger.debug(("-"*5)+"Point"+("-"*5))
             driver.get(f"https://reddit.com/r/{rep}")
 
             driver.switch_to.default_content()
-            logger.debug(("-"*5)+"Point"+("-"*5))
             
             play_videos(driver.find_elements(By.CSS_SELECTOR, 'iframe.media-element'), common=driver)
-            
-            logger.debug(("-"*5)+"Point"+("-"*5))
-            
+             
             if rep != self.active_subreddits[-1]: driver.switch_to.new_window()
             
-            logger.debug(("-"*5)+"Point"+("-"*5))
         
         logger.info("Starting MainLoop")
         import time
@@ -77,10 +78,16 @@ class Bot(__threading.Thread):
             for tab in driver.window_handles:
                 driver.switch_to.window(tab)
 
-                driver.execute_script("document.querySelectorAll('iframe.media-element')[0].src = document.querySelectorAll('iframe.media-element')[0].src")
-                logger.debug("reloading youtube iframe")
+                for frame in driver.find_elements(By.CSS_SELECTOR, 'iframe.media-element'):
+                    driver.execute_script("arguments[0].src = arguments[0].src", frame)
+                
+                logger.debug("reloaded youtube iframe")
 
                 driver.switch_to.default_content()
                 play_videos(driver.find_elements(By.CSS_SELECTOR, 'iframe.media-element'), common=driver)
 
                 logger.debug("waiting for 40 seconds")
+        
+        logger.warning("Ending of a While True loop has been reached")
+        logger.warning("Under Normal Surcomestances this should be impossible")
+        return None
